b413ec06ec313a8684fcc09cb7d741e52c3a3e5c
Eduard Toloza <tolosaeduard@gmail.com>
2019-01-26 10:15:55 -0500

    Fix installer

--- install.sh
+++ install.sh
@@ -4,25 +4,25 @@ ROOT_UID=0
 
 function webhackinstall() {
 	if [ -f /etc/debian_version ] ; then
-		if [ -f /usr/bin/webhackshl -a -f /opt/webhackshl/webhackshl.py ] ; then
+		if [ -f /usr/bin/webhackshl -a -f /opt/webhackshl/webhackshl ] ; then
 			echo -e "\nWebHackSHL ya está instalado en el sistema, no necesita reinstalarse."
 		else
 			echo -e "Procediendo con la instalación en tu sistema Debian o basado en Debian."
 			mkdir /opt/webhackshl/
 			cp -R * /opt/webhackshl/
-			echo -e "\n#!/usr/bin/bash\npython /opt/webhackshl/webhackshl.py" | sudo tee -a /usr/bin/webhackshl > /dev/null
+			echo -e "\n#!/usr/bin/bash\npython /opt/webhackshl/webhackshl" | sudo tee -a /usr/bin/webhackshl > /dev/null
 			chmod +x /usr/bin/webhackshl
 			echo -e "Instalación finalizada, ahora puedes ejecutar el comando "webhackshl" directamente desde tu terminal.\n"
 		fi
 
 	elif [ -f /etc/arch-release ] ; then
-		if [ -f /usr/bin/webhackshl -a -f /opt/webhackshl/webhackshl.py ] ; then
+		if [ -f /usr/bin/webhackshl -a -f /opt/webhackshl/webhackshl ] ; then
             echo -e "\nWebHackSHL ya está instalado en el sistema, no necesita reinstalarse."
 		else
 			echo -e "Procediendo con la instalación en tu sistema ArchLinux o basado en ArchLinux."
             mkdir /opt/webhackshl/
             cp -R * /opt/webhackshl/
-			echo -e "\n#!/usr/bin/bash\npython2 /opt/webhackshl/webhackshl.py" | sudo tee -a /usr/bin/webhackshl > /dev/null
+			echo -e "\n#!/usr/bin/bash\npython /opt/webhackshl/webhackshl.py" | sudo tee -a /usr/bin/webhackshl > /dev/null
 			chmod +x /usr/bin/webhackshl
 			echo -e "Instalación finalizada, ahora puedes ejecutar el comando "webhackshl" directamente desde tu terminal.\n"
 		fi
